a0472edd67e02f71a54fd33a5093cb63558fd3f4
SamGreensill <samgreensill@aol.co.uk>
2024-05-27 21:10:40 +0100

    Opening of new directory

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,11 @@
 // Name is depriciated so make it as function 
+// NOTE Switching betweem termial directorys is 'Cd then where you want to be.
 (function () {
-  
-const getNotes = require('./notes.js!');
+    const validator = require('validator');
+    const getNotes = require('./notes.js'); // Corrected path
 
-const msg = getNotes()
+    const msg = getNotes();
+    console.log(msg);
 
-console.log(msg)
-
+    console.log(validator.isEmail('Samgreensill@aol.co.uk'));
 })();
