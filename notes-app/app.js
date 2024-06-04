806e1bd69c613165b2059404def83d5e69d637ad
SamGreensill <samgreensill@aol.co.uk>
2024-06-04 20:24:51 +0100

    Cleaning up of files

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,5 @@
 // Name is depriciated so make it as function 
-// NOTE Switching betweem termial directorys is 'Cd then where you want to be.
+// NOTE Switching betweem termial directorys is 'Cd then where you want to be
 (function () {
     const validator = require('validator');
     const getNotes = require('./notes.js'); // Corrected path
