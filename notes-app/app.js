5f3dd4de1cf8eaafb09f5ee46ff0efde1c85b10f
SamGreensill <samgreensill@aol.co.uk>
2024-06-17 19:41:24 +0100

    Creation of application commands

--- notes-app/app.js
+++ notes-app/app.js
@@ -3,17 +3,52 @@ async function displayNotes() {
     const yargs = require('yargs')
     const getNotes = (await import('./notes.js')).default;
 
-  const command = process.argv [2]
 
-console.log(process.argv)
+//Customize yargs version 
+yargs.version('1.1.0')
 
-  if (command === 'add') {
-    console.log("Adding note")
+//addition, reading, read, list
 
-  } else if (command === 'remove') {
-    console.log('Removing note')
-  }
+//Creation of add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log('Adding a new note!')
+    }
+})
+
+//Creation of remove command
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Removing the note!')
+    }
+})
+
+//Creation of list command 
+
+yargs.command({
+    command: 'list',
+    describe: 'List some data',
+    handler: function() {
+        console.log('Listing information')
+    }
+})
+
+//Creation of read command 
+
+yargs.command({
+    command: 'read',
+    describe: 'Reading of data',
+    handler: function() {
+        console.log('Reading information')
+    }
+})
+
+console.log(yargs.argv)
+
 }
-
-
 displayNotes();
